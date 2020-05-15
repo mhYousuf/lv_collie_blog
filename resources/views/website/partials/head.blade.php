fae915f030c5110ae1e222d909d047bd6d440af4
Mehediul Hassan Miton <mehediul.dev@gmail.com>
2020-05-15 11:00:25 +0600

    blogde

--- resources/views/website/partials/head.blade.php
+++ resources/views/website/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Blogde</title>
+<title>BlogDe</title>
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CMuli:400,700" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/website/css/bootstrap.min.css')}}" />
 <link rel="stylesheet" href="{{ asset('assets/website/css/font-awesome.min.css')}}">
